32291ad216792662f742860e64bb2844e3c3192a
adityathakur2008 <adityathakur353465>
2026-08-02 20:57:05 +0530

    fix api check route

--- server/src/routes/apiCheck.js
+++ server/src/routes/apiCheck.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const apiCheckRouter = Router();
 
-apiCheckRouter.get("/check", (req, res) => {
+apiCheckRouter.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "API is working" });
 });
 
